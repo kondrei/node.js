58e50df18c1ba3edb491e000415cd8f8ca9b43ca
kondrei <kondrei@gmail.com>
2021-08-27 01:34:45 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const validator = require('validator');
+const getNotes = require('./notes.js');
+const chalk = require('chalk');
+const yargs = require('yargs');
+
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    handler: function() {
+        console.log('adding new note');
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove note',
+    handler: function() {
+        console.log('remve note');
+    }
+})
+
+yargs.command({ 
+    
+    command: 'list',
+    describe: 'list notes',
+    handler: function() {
+        console.log('listing notes');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read note',
+    handler: function() {
+        console.log('read note');
+    }
+})
+
+console.log(yargs.argv)
